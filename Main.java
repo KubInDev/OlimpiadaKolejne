28d1feaf493e47a90185e1ffdc13056535d24483
KubInDev <54432043+KubInDev@users.noreply.github.com>
2020-02-15 18:20:53 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,9 @@
  */
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
@@ -27,6 +29,7 @@ public class Main {
         String jasNazwisko = scanner.next();
         String malgosiaNazwisko = scanner.next();
 
+
         if(liczbaLiterNieprawidlowa(liczbaLiter)){
             throw new IllegalArgumentException("Liczba liter nie miesci sie w warunku: 2<=n<=1000000");
         }
@@ -40,11 +43,71 @@ public class Main {
             throw new IllegalArgumentException("Nazwisko Jasia i Malgosi nie uzywa tych samych liter");
         }
 
-        while(!(jasNazwisko==malgosiaNazwisko)) {
+        System.out.println("Nazwisko Jasia: " + jasNazwisko);
+        System.out.println("Nazwisko Malgosi: " + malgosiaNazwisko);
+        System.out.println("------------");
+        char[] literyWNazwiskuJasia = new char[liczbaLiter];
+        literyWNazwiskuJasia = jasNazwisko.toCharArray();
+        char[] literyWNazwiskuMalgosi = new char[liczbaLiter];
+        literyWNazwiskuMalgosi = malgosiaNazwisko.toCharArray();
+        char[] doNaprawieniaLitery = new char[liczbaLiter];
+        char[] dobreLitery = new char[liczbaLiter];
+        int przelozenia = 0;
 
+
+        while(!(Arrays.equals(literyWNazwiskuJasia, literyWNazwiskuMalgosi))) {
+            for (int x = 0; liczbaLiter - 1 >= x; x++) {
+
+                if(!(literyWNazwiskuJasia[x]==literyWNazwiskuMalgosi[x])) {
+                    int miejsceLiteryUJasia = x+1;
+                    doNaprawieniaLitery[x]=literyWNazwiskuJasia[x];
+                    System.out.println(doNaprawieniaLitery[x] + "; Ta litera jest na miejscu: " + miejsceLiteryUJasia);
+                    for(int z=0; z<=liczbaLiter - 1; z++) {
+                        if(doNaprawieniaLitery[x]==literyWNazwiskuMalgosi[z]) {
+                            if(!(literyWNazwiskuMalgosi[z]==literyWNazwiskuJasia[z])) {
+                                int miejsceLiteryUMalgosi = z+1;
+                                System.out.println("Litera odpowiadająca jest na: " + miejsceLiteryUMalgosi + " miejscu.");
+                                while(!(literyWNazwiskuMalgosi[miejsceLiteryUMalgosi-1]==literyWNazwiskuJasia[miejsceLiteryUMalgosi-1])) {
+                                    if(czyWPrawo(miejsceLiteryUJasia, miejsceLiteryUMalgosi)) {
+                                        int oIleWPrawo = miejsceLiteryUMalgosi-miejsceLiteryUJasia;
+                                        for(int p=1; p<=oIleWPrawo; p++ ) {
+                                            literyWNazwiskuJasia=swap(jasNazwisko, p-1, p);
+                                            przelozenia++;
+                                        }
+                                    } else {
+                                        int oIleWLewo = miejsceLiteryUJasia-miejsceLiteryUMalgosi;
+                                        for(int p=1; p<=oIleWLewo; p++) {
+                                            literyWNazwiskuJasia=swap(jasNazwisko, p, p-1);
+                                            przelozenia++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    dobreLitery[x] = literyWNazwiskuJasia[x];
+                }
+
+            }
         }
+        System.out.println("Ulozone w " + przelozenia);
+        for (int x = 0; liczbaLiter - 1 >= x; x++) {
+            System.out.println(literyWNazwiskuJasia[x]);
+        }
+    }
 
 
+    static char[] swap(String str, int i, int j) {
+        char ch[] = str.toCharArray();
+        char temp = ch[i];
+        ch[i] = ch[j];
+        ch[j] = temp;
+        return ch;
+    }
+
+    private static boolean czyWPrawo(int miejsceLiteruUJasia, int miejsceLiteryUMalgosi) {
+        return miejsceLiteruUJasia < miejsceLiteryUMalgosi;
     }
 
     private static boolean takieSameLiteryWNazwiskach(String jasNazwisko, String malgosiaNazwisko) {
@@ -52,16 +115,14 @@ public class Main {
         char[] drugie = malgosiaNazwisko.toCharArray();
         Arrays.sort(pierwsze);
         Arrays.sort(drugie);
-        if(Arrays.equals(pierwsze, drugie)){
-            return false;
-        } else {
-            return true;
-        }
+        return !Arrays.equals(pierwsze, drugie);
     }
+
 
     private static boolean liczbaLiterNieprawidlowa(int liczbaLiter) {
         return (liczbaLiter < DOLNA_GRANICA || liczbaLiter > GORNA_GRANICA);
     }
+
 
     private static boolean dlugoscNazwiskaNieprawidolowa(int liczbaLiter, String nazwisko) {
         if(liczbaLiter>nazwisko.length() || liczbaLiter<nazwisko.length()) {
